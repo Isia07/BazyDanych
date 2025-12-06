9f0770985c140174a2ebf1a5b7414d6a20325989
Isia07 <aga.p.10@wp.pl>
2025-12-06 23:14:56 +0100

    discount post and get

--- EventTickets/relational/views.py
+++ EventTickets/relational/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from EventTickets.relational.models import Discount
+from EventTickets.relational.serializers import DiscountSerializer
 from EventTickets.shared.views import BaseRegisterView, BaseLoginView
 
 class RelRegisterView(BaseRegisterView):
     database = 'relational'
 
+
 class RelLoginView(BaseLoginView):
     database = 'relational'
+
+
+class DiscountListCreateView(generics.ListCreateAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = DiscountSerializer
+
+
+class DiscountDetailView(generics.RetrieveAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = DiscountSerializer
+
--- EventTickets/urls.py
+++ EventTickets/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from EventTickets.objective_relational.views import RegisterView as ObjRelRegister, LoginView as ObjRelLogin
-from EventTickets.relational.views import RelRegisterView, RelLoginView
+from EventTickets.relational.views import RelRegisterView, RelLoginView, DiscountListCreateView, DiscountDetailView
 
 urlpatterns = [
     path('api/v1/obj-rel/auth/register', ObjRelRegister.as_view(), name='obj_rel_register'),
     path('api/v1/obj-rel/auth/login', ObjRelLogin.as_view(), name='obj_rel_login'),
     path('api/v1/rel/auth/register', RelRegisterView.as_view(), name='rel_register'),
     path('api/v1/rel/auth/login', RelLoginView.as_view(), name='rel_login'),
+    path('api/v1/rel/discounts/', DiscountListCreateView.as_view(), name='discount_list_create'),
+    path('api/v1/rel/discounts/<int:pk>/', DiscountDetailView.as_view(), name='discount_detail'),
 
 ]
