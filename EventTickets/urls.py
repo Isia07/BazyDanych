b183b4c3e93616f45ee8b1f3790105eb32fc723a
Mateusz Horczak <82621@student.pb.edu.pl>
2025-12-06 13:40:51 +0100

    refactor(urls): Move urls from config to app.

--- EventTickets/urls.py
+++ EventTickets/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from EventTickets.objective_relational.views import RegisterView as ObjRelRegister, LoginView as ObjRelLogin
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('select_database/', views.select_database, name='select_database'),
+    path('api/v1/obj-rel/auth/register', ObjRelRegister.as_view(), name='obj_rel_register'),
+    path('api/v1/obj-rel/auth/login', ObjRelLogin.as_view(), name='obj_rel_login'),
 
 ]
