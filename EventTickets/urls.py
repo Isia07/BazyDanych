962a36d26fe4f5858e3d8df88e3b3ef100b88e38
konradTOP <konrad.topczewski@wp.pl>
2025-12-07 22:37:13 +0100

    urls changes

--- EventTickets/urls.py
+++ EventTickets/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from . import views
+from EventTickets.nosql.views import (
+    nosql_events_list,
+    nosql_users_list,
+    nosql_orders_list,
+    nosql_notifications_list,
+    nosql_messages_list,
+)
 
 from .objective.views import (
     DiscountsDetailView as ObjDiscountsDetailView,
@@ -99,6 +107,14 @@ from .relational.views import (
 )
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('select_database/', views.select_database, name='select_database'),
+
+path('api/nosql/events/', nosql_events_list, name='nosql-events-list'),
+    path('api/nosql/users/', nosql_users_list, name='nosql-users-list'),
+    path('api/nosql/orders/', nosql_orders_list, name='nosql-orders-list'),
+    path('api/nosql/notifications/', nosql_notifications_list, name='nosql-notifications-list'),
+    path('api/nosql/messages/', nosql_messages_list, name='nosql-messages-list'),
     # register
     path(
         "api/v1/obj-rel/auth/register/",
